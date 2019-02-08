ed2c9cd1507ff8343b6a9db2969cf2b7a3759f4f
DESKTOP-42LSCSC\shenl <shenlong_12@hotmail.com>
2019-02-08 15:27:51 -0400

    feat: vue cli vuetify added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import axios from 'axios'
 import VueRouter from 'vue-router'
 import VueAxios from 'vue-axios'
@@ -6,8 +7,11 @@ import VueAxios from 'vue-axios'
 import router from './routes'
 
 import Vuetify from 'vuetify'
-
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import './assets/scss/app.scss'
 import App from './App.vue'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.use(VueAxios, axios)
 Vue.use(VueRouter);
--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,6 @@
-const path = require("path");
 module.exports = {
-  runtimeCompiler: true,
-
-  css: {
-    modules: true
-  },
-
-  publicPath: undefined,
-  outputDir: 'public/dist/',
-  assetsDir: 'public/',
-  productionSourceMap: undefined,
-  parallel: undefined,
-
-  pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: [path.resolve(__dirname, "./src/assets/scss/app.scss")]
-    }
-  }
+	runtimeCompiler: true,
+	css: {
+		modules: true
+	}
 }
