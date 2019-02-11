2bfa5e7a574c09c72fbb035ac517609c91e9cbe8
DESKTOP-42LSCSC\shenl <shenlong_12@hotmail.com>
2019-02-11 14:49:16 -0400

    feat: App component and css added

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,17 @@
 <template>
-  <div id="app">
+  <v-app id="app">
     <router-view></router-view>
-  </div>
+  </v-app>
 </template>
 
 <script>
 
 export default {
-  name: 'app'
+  name: 'app',
   
 }
 </script>
 
-<style >
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+<style>
+
 </style>
