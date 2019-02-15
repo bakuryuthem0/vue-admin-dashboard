e3983b1912d4f9d931f4ba1aa3b1a6aeffcc73c2
DESKTOP-42LSCSC\shenl <shenlong_12@hotmail.com>
2019-02-15 17:27:22 -0400

    feat: custom route added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,25 @@
 import VueRouter from 'vue-router'
 import Login from '../components/Views/Layouts/Login'
+import Dashboard from '../components/Views/Layouts/Dashboard'
+import ShowClients from '../components/Views/Clients/ShowClients'
 
 const router = new VueRouter({
 	routes: [{
 		path: '/',
 		component: Login,
-		name: 'Login',
-		meta: {
-			auth: false
-		}
+		name: 'Login'
+    },
+    {
+    	path: '/dashboard',
+    	component: Dashboard,
+    	name: 'dashboard',
+    	children : [
+    		{
+    			path: 'clients',
+    			component: ShowClients,
+    			name: 'ShowClients'
+    		}
+    	]
     }]
 })
 
