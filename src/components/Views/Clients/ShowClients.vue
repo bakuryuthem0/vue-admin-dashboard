d65437cde79191a55ac0a8bd8fbac1fe82bfd593
DESKTOP-42LSCSC\shenl <shenlong_12@hotmail.com>
2019-02-15 17:28:26 -0400

    feat: custom view added

---
+++ src/components/Views/Clients/ShowClients.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-flex s12>
+		holi
+	</v-flex>
+</template>
+<script>
+	export default {
+		data: function() {
+			return {
+				clients: []
+			}
+		},
+		created: function() {
+			this.fetch();
+		},
+		methods: {
+			async fetch() {
+				this.clients = this.$http.get('oauth/clients')
+			}
+		},
+		watch: {
+			'$route': 'fetch'
+		}
+	}
+</script>
